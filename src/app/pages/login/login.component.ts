e38c64926fcb696cf93338f9d2a9463d4821fdc6
Victor Bruce <victorbruce82@gmail.com>
2025-06-25 08:20:39 +0000

    feat: add login form to login page and implement sign in with email and password functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,54 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   private auth = inject(AuthService);
+  private router = inject(Router);
+  private fb: FormBuilder = inject(FormBuilder);
+  loginForm!: FormGroup;
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: this.fb.control('', [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(5),
+      ]),
+      password: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
 
   async handleAuth() {
     const response = await this.auth.signInWithGoogle();
+  }
+
+  onSubmit() {
+    this.auth
+      .signIn(this.loginForm.value.email, this.loginForm.value.password)
+      .then((res) => {
+        console.log('login form', res);
+        if (res.data.user?.role === 'authenticated') {
+          this.router.navigate(['/notes']);
+        }
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
   }
 }
