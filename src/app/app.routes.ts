8598c49c44a8b3b451abb098a6ee5df7a0404621
Victor Bruce <victorbruce82@gmail.com>
2025-06-26 16:34:28 +0000

    refactor: clean up app routes to use lazy loading for some page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,45 +1,43 @@
 import { Routes } from '@angular/router';
 
 // pages
-import { NotesComponent } from './pages/notes/notes.component';
-import { ArchivedComponent } from './pages/archived/archived.component';
-import { AddNoteComponent } from './pages/add-note/add-note.component';
-import { NoteDetailComponent } from './pages/note-detail/note-detail.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { authGuard } from './guards/auth.guard';
+import { NotesComponent } from './pages/notes/notes.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'notes',
     component: NotesComponent,
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
     canActivate: [authGuard],
-  },
-  {
-    path: 'notes/:id',
-    component: NoteDetailComponent,
-  },
-  {
-    path: 'archived',
-    component: ArchivedComponent,
-  },
-  {
-    path: 'create',
-    component: AddNoteComponent,
+    children: [
+      {
+        path: 'notes',
+        loadChildren: () =>
+          import('./pages/notes/notes.component').then((c) => c.NotesComponent),
+      },
+      {
+        path: 'archived',
+        loadChildren: () =>
+          import('./pages/archived/archived.component').then(
+            (c) => c.ArchivedComponent
+          ),
+      },
+      {
+        path: 'create',
+        loadChildren: () =>
+          import('./pages/new-note/new-note.component').then(
+            (c) => c.NewNoteComponent
+          ),
+      },
+    ],
   },
   { path: '**', component: PageNotFoundComponent },
 ];
